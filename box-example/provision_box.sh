ad801c08e631dce0d9d60a0b2b9724b1890048e3
Vyacheslav Voronenko <git@voronenko.info>
2019-12-13 11:35:12 +0100

    adjusted template

--- box-example/provision_box.sh
+++ box-example/provision_box.sh
@@ -1,26 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 # Static parameters
-
-REMOTE_HOST=188.166.79.9
-BOX_DEPLOY_USER=root
-
 WORKSPACE=$(
   cd $(dirname "$0")
   pwd
 )
 
+#source $WORKSPACE/../../circleci_mapper.sh
+
 BOX_PLAYBOOK=$WORKSPACE/box.yml
-BOX_NAME=test_box
-BOX_ADDRESS=$REMOTE_HOST
-BOX_USER=$BOX_DEPLOY_USER
+BOX_NAME=demo_box
+BOX_ADDRESS=${REMOTE_HOST:-specify}
+BOX_USER=${BOX_DEPLOY_USER:-ubuntu}
 BOX_PWD=$BOX_DEPLOY_PASS
 BOX_PROVIDER=${BOX_PROVIDER:-}
 ENVIRONMENT=${ENVIRONMENT:-default}
 
-
 prudentia ssh <<EOF
 unregister $BOX_NAME
 
+# add for sudoered box
+# set ansible_become_password $BOX_PWD
 register
 $BOX_PLAYBOOK
 $BOX_NAME
@@ -29,10 +28,13 @@ $BOX_USER
 $BOX_PWD
 
 verbose 1
+
 set box_address $BOX_ADDRESS
-set ansible_become_password $BOX_PWD
+
 set box_provider $BOX_PROVIDER
 set env $ENVIRONMENT
+set git_branch ${CIRCLE_BRANCH:-undetected_git_branch}
+
 
 provision $BOX_NAME
 EOF
